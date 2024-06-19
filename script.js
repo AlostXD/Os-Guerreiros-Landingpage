dbd917a9e1e0bfd2407daa98636a93a5da4fab9d
Ketley-Souza <ketleysouza55@gmail.com>
2024-06-19 19:18:39 -0300

    Feito a div de Servidores

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function closeSide(){
     sidebar.style.display = 'none';
 }
 
+//Modal
+
+const button = document.querySelector("button");
+const modal = document.querySelector("dialog");
+const buttonClose = document.querySelector("dialog button");
+
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.showModal();
+    } else {
+        console.error('Modal não encontrado:', modalId);
+    }
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.close();
+    } else {
+        console.error('Modal não encontrado:', modalId);
+    }
+}
